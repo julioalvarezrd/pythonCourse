6b61d9daaa18fbfd8b783d5e6ceff1cbe5315e84
Julio Alvarez <julioalvarezrd@gmail.com>
2023-07-01 19:22:26 -0400

    Tipos basicos: Modulo Math

---
+++ 07-modulo-math.py
@@ -0,0 +1,10 @@
+import math  # importando la libreria o modulo math
+
+print(math.ceil(1.1))  # Nos lleva al numero entero mas cercano hacia arriba
+print(math.floor(1.9999))  # Nos lleva al numero entero mas cercano hacia abajo
+print(math.pow(5, 3))  # Nos permite elevar un numero a la potencia de algo
+print(math.sqrt(9))  # Nos saca la raiz cuadrada
+
+# Funciones nativas para trabajar con numeros
+print(round(1.6))  # retorna el numero entero mas cercano.
+print(abs(-54))  # nos devuelve el valor absoluto.
