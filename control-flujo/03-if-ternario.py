a980c73ade5e9558949130e1f51d0bad6a9e1d1e
Julio Alvarez <julioalvarezrd@gmail.com>
2023-07-01 22:55:59 -0400

    Operador Ternario

---
+++ control-flujo/03-if-ternario.py
@@ -0,0 +1,14 @@
+edad = 15
+
+# if clasico
+if edad > 17:
+    mensaje = "Es Mayor"
+else:
+    mensaje = "Es Menor"
+
+print(mensaje)
+
+# Operador Ternario
+edad = 18
+mensaje = "Es Mayor" if edad > 17 else "Es menor"
+print(mensaje)
