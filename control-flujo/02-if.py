467d07715d5699de131459279eccf48989b33f53
Julio Alvarez <julioalvarezrd@gmail.com>
2023-07-01 22:47:34 -0400

    # if, elif y else de manera grafica

--- control-flujo/02-if.py
+++ control-flujo/02-if.py
@@ -5,3 +5,13 @@ elif edad >= 18:
     print("Puedes ver la pelicula")
 else:
     print("No Puedes entrar")
+
+# if, elif y else de manera grafica
+if edad > 65:
+    print("Super Descuento")
+elif edad > 55:
+    print("Descuento")
+elif edad > 17:
+    print("Puede Entrar")
+else:
+    print("Debes irte")
