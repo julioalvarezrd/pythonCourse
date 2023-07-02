72fa1136b74cc15c4320639f9322adaadec24370
Julio Alvarez <julioalvarezrd@gmail.com>
2023-07-01 22:28:37 -0400

    Comparadores Logicos

---
+++ control-flujo/01-comparadores.py
@@ -0,0 +1,11 @@
+print(1 > 2)  # Mayor que False
+print(1 < 2)  # Menmor True
+print(1 >= 2)  # Mayor igual False
+print(1 <= 2)  # Menor igual True
+print(2 >= 2)  # Mayor igual True
+print(2 <= 2)  # Menor igual True
+print(1 == 2)  # Igual False
+print(2 == 2)  # Igual True
+print(2 == "2")  # Igual False
+print(2 != 2)  # Distinto False
+print(2 != "2")  # Distinto True
