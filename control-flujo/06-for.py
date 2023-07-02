a2ab359784c56702b0f5d33307a9893974537e46
Julio Alvarez <julioalvarezrd@gmail.com>
2023-07-01 23:32:30 -0400

    For Else

--- control-flujo/06-for.py
+++ control-flujo/06-for.py
@@ -2,3 +2,13 @@
 
 for numero in range(5):
     print(numero, numero * 'hola mundo: ')
+
+# probando for else
+buscar = 2
+for i in range(5):
+    print(i)
+    if i == buscar:
+        print("Encontrado", buscar)
+        break
+else:
+    print("No encontrado")
