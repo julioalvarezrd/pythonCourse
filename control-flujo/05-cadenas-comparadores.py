a80edd9506bd9e8ff659c739fc86ef1cac53765a
Julio Alvarez <julioalvarezrd@gmail.com>
2023-07-01 23:16:39 -0400

    Cadenas de copmparacion

---
+++ control-flujo/05-cadenas-comparadores.py
@@ -0,0 +1,10 @@
+edad = 25
+# Forma Clasica
+if edad >= 15 and edad <= 65:
+    print("puede entrar a la piscina")
+
+# Forma resumida
+if 15 <= edad <= 65:
+    print("puede entrar a la piscina")
+
+    
